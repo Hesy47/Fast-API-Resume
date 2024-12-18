b6fa55b9cc6b7273f2a9b9768e73ab8a16de01b2
Hesam <death19knight@gmail.com>
2024-12-18 11:49:05 +0330

    Initial Base

--- validations.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-
-class Create_User(BaseModel):
-    username: str
-    password: str = Field(None, min_length=6, description="test case")
-    email: str
-    age: Optional[int]
